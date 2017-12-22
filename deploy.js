9a221c94236d64bfa1a669bbd7c961bcf65b6981
John Smith <themtrx@protonmail.com>
2017-12-22 11:17:47 +0200

    fix

--- deploy.js
+++ deploy.js
@@ -7,24 +7,24 @@ for (let i in cfg.netTune) netTuned = netTuned.concat(cfg.netTune[i]);
 fs.writeFileSync(cfg.netTunePath, netTuned);
 netTuned = undefined;
 
-let nginx = fs.readFileSync(cfg.nginxInput);
+let nginx = fs.readFileSync(cfg.nginxInput, 'utf8');
 nginx = nginx.split('$$uwsuri').join(`${cfg.server.domain}:${cfg.server.port}`);
 nginx = nginx.split('$$appport').join(cfg.server.appPort);
 nginx = nginx.split('$$appdomain').join(cfg.server.domain);
 nginx = nginx.split('$$libRoot').join(cfg.libRoot);
 nginx = nginx.split('$$webRoot').join(cfg.webRoot);
 nginx = nginx.split('$$proxyProto').join(cfg.ssl.enabled ? 'https' : 'http');
-fs.writeFileSync(cfg.nginxPath, nginx);
+fs.writeFileSync(cfg.nginxPath, nginx, 'utf8');
 nginx = undefined;
 
-let worker = fs.readFileSync(`${cfg.webRoot}/${cfg.workerFilename}`);
+let worker = fs.readFileSync(`${cfg.webRoot}/${cfg.workerFilename}`, 'utf8');
 worker = worker.split('$$endpoint').join(cfg.server.uri);
 worker = worker.split('$$wsEndpoint').join(cfg.server.wsuri);
-fs.writeFileSync(`${cfg.webRoot}/${cfg.workerFilename}`, worker);
+fs.writeFileSync(`${cfg.webRoot}/${cfg.workerFilename}`, worker, 'utf8');
 worker = undefined;
 
-let miner = fs.readFileSync(`${cfg.webRoot}/${cfg.minerFilename}`);
+let miner = fs.readFileSync(`${cfg.webRoot}/${cfg.minerFilename}`, 'utf8');
 miner = miner.split('$$endpoint').join(cfg.server.uri);
 miner = miner.split('$$wsEndpoint').join(cfg.server.wsuri);
-fs.writeFileSync(`${cfg.webRoot}/${cfg.minerFilename}`, miner);
+fs.writeFileSync(`${cfg.webRoot}/${cfg.minerFilename}`, miner, 'utf8');
 miner = undefined;
