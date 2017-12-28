460e6bb54dddc87b0428584a31d4afe060e8b208
John Smith <themtrx@protonmail.com>
2017-12-28 19:08:18 +0200

    optimized

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -1,9 +1,38 @@
-#!/bin/sh
+9#!/bin/sh
 
 echo "Welcome to uDeepMiner installation script!"
 echo ""
 
 NEED_NODE=false
+INSTALL_PATH="$1"
+RUNNER_UID=`id -u`
+USERNAME=`whoami`
+
+if [ $RUNNER_UID != 0 ]
+then
+    echo "!!! Run as superuser to proceed!"
+    exit 0
+fi
+
+if [ "$SUDO_USER" = `id -un 0` ]
+then
+    echo "!!! BE TOO CAREFUL WHEN INSTALLING AS ROOT !"
+else
+    USERNAME=$SUDO_USER
+fi
+
+GROUPNAME=`sudo -u $USERNAME id -gn $USERNAME`
+
+if [ "$INSTALL_PATH" = '' ]
+then
+    INSTALL_PATH="/home/$USERNAME/.local"
+    if [ ! -d "$INSTALL_PATH" ]
+    then
+        mkdir --mode a=rx,u+w $INSTALL_PATH
+        chown -R $USERNAME:$GROUPNAME $INSTALL_PATH
+    fi
+fi
+echo "... installing as $USERNAME to $INSTALL_PATH"
 
 if [ ! -n "$NODE_VERSION" ]
 then
@@ -11,12 +40,12 @@ then
 fi
 
 echo "... checking whether Node.js ${NODE_VERSION} is installed"
-NODE_LOCATION=`which node`
+NODE_LOCATION=`sudo -u $USERNAME which node`
 if [ "$NODE_LOCATION" = '' ]
 then
     NEED_NODE=true
 else
-    EXISTING_NODE_VERSION=`node -v`
+    EXISTING_NODE_VERSION=`sudo -u $USERNAME node -v`
     if [ "$EXISTING_NODE_VERSION" != "$NODE_VERSION" ]
     then
         NEED_NODE=true
@@ -24,28 +53,53 @@ else
 fi
 if [ "$NEED_NODE" = true ]
 then
-    echo "... installing fresh Node.js, nginx and build-essential"
-    curl -#L https://deb.nodesource.com/setup_9.x | sudo -E bash -
-    sudo apt-get install -y nodejs nginx build-essential
+    echo "... installing fresh Node.js"
+    sudo -u $USERNAME curl -#L https://deb.nodesource.com/setup_9.x | sudo -E bash -
+    apt-get install -y nodejs
 fi
+
+echo "... installing build libs and nginx"
+apt-get install -y nginx build-essential clang g++
+
 echo "... updating npm"
-sudo npm update -g npm
+npm update -g npm
 
 echo "... installing pm2"
-sudo npm i -g pm2
+npm i -g pm2
 
-echo "... fetching app"
-git clone -o uDeepMiner https://github.com/perimetral/uDeepMiner.git
-cd uDeepMiner
+echo "... fetching app to $INSTALL_PATH"
+sudo -u $USERNAME git clone https://github.com/perimetral/uDeepMiner.git $INSTALL_PATH/uDeepMiner
+
+if [ -f "../config.js" ]
+then
+    echo "... updating configuration"
+    sudo -u $USERNAME rm -rf $INSTALL_PATH/uDeepMiner/config.js
+    cp ../config.js ./config.new.js
+    chown $USERNAME:$GROUPNAME ./config.new.js
+    sudo -u $USERNAME cp ./config.new.js $INSTALL_PATH/uDeepMiner/config.js
+    rm -rf ./config.new.js
+fi
+
+cd $INSTALL_PATH/uDeepMiner
 
 echo "... installing deps"
-npm i
+sudo -u $USERNAME npm i
+echo "npm i uws" | sudo -u $USERNAME -E sh -
+
+if [ ! -f "/etc/nginx/nginx.conf" ]
+then
+    touch /etc/nginx/nginx.conf
+fi
 
 echo "... configuring sysctl, nginx and clientside"
-sudo node ./deploy.js
+node ./deploy.js
 
-echo "... running Node.js"
-node ./server.js
+echo "... fixing permissions issues"
+cd ..
+chown -R $USERNAME:$GROUPNAME uDeepMiner
 
-echo "... enabling nginx"
-sudo systemctl start nginx
+cd ./uDeepMiner
+echo "... running servers"
+systemctl reload nginx
+systemctl start nginx
+sudo -u $USERNAME node ./server.js &
